38628069eef1bbfc8577052ea47206e066c947d3
0xB10C <19157360+0xB10C@users.noreply.github.com>
2023-10-04 19:15:49 +0000

    warn about using nixos-rebuild in the VM

--- common.nix
+++ common.nix
@@ -24,12 +24,6 @@ in
     "nixos-config=/etc/nixos/rebuild.nix"
   ];
 
-  # alias nixos-rebuild switch to nixos-rebuild switch with verbose output
-  # this helps to show that there's still progress and we're not stuck..
-  environment.interactiveShellInit = ''
-    alias nixos-rebuild='nixos-rebuild --verbose -vv --fast'
-  '';
-
   # enable ssh access to the VM
   services.openssh.enable = true;
 
--- vm-codespaces.nix
+++ vm-codespaces.nix
@@ -8,4 +8,8 @@
     cores = 1;
     memorySize = 4 * 1024;
   };
+
+  environment.interactiveShellInit = ''
+    alias nixos-rebuild='echo "Due to the VM being emulated, everything is quite slow in here. Do not run nixos-rebuild in the VM. Use   sh nixos-rebuild-vm.sh   from the host for this workshop."'
+  '';
 }
